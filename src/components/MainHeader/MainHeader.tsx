3c26aea2913fd56a67ec7177e7e808cf6997df13
kevin-krause <contatokevinkrause@gmail.com>
2024-02-12 22:30:11 -0300

    scss main header

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AsyncButton from '../AsyncButton'
 import { IButtonProps } from '../AsyncButton/AsyncButton'
 import DropDownMenu from '../DropDrownMenu'
+import './MainHeader.scss'
 
 const dropDownItems = [
     {
@@ -21,11 +22,13 @@ const MainHeader: React.FC<IButtonProps> = props => {
     const { title, onclick, icon } = props
     return (
         <div className="main-header">
-            <>
+            <div className="title-container">
                 <h1 className="main-title">Chats</h1>
                 <DropDownMenu optionProps={dropDownItems} />
-            </>
-            <AsyncButton title={title} onclick={onclick} icon={icon} />
+            </div>
+            <div className="button-container">
+                <AsyncButton title={title} onclick={onclick} icon={icon} />
+            </div>
         </div>
     )
 }
